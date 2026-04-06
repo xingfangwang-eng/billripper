05eb19d3d6afaa6b6f385ed19e73343d116cb015
wangxf0892 <50897719+wangxf0892@users.noreply.github.com>
2026-04-06 08:07:26 +0800

    Add Google Analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WC4677QJMF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  // 动态注入项目名
+  gtag('config', 'G-WC4677QJMF', {
+    'project_name': 'billripper'
+  });
+</script>
       <body className="grid-gradient min-h-screen flex flex-col">
         {children}
       </body>
